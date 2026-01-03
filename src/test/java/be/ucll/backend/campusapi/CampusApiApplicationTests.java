03c04d3f265b8f3bddbb662822f2226598b7c2c1
Daniel <ferndani00@gmail.com>
2026-01-03 11:06:25 +0100

    Initial commit

---
+++ src/test/java/be/ucll/backend/campusapi/CampusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.backend.campusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
